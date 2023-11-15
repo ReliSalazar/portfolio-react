41a817c61a510d9401e94ae2994a1eca342581ff
Reli <naitvenefy@gmail.com>
2023-11-15 13:18:13 -0600

    blog post on separated files

---
+++ src/services/blog/moreAboutMe.js
@@ -0,0 +1,35 @@
+export const moreAboutMe = {
+  id: "about",
+  title: "More about me",
+  date: "April 3, 2022",
+  editDate: "",
+  author: "Reli",
+  img: "/BlogImages/about/selfie-1.webp",
+  body: [
+    {
+      contentType: "paragraph",
+      content:
+        "Hello, I'm Reli Salazar and I am from Mexico.\nSince I can remember, I have always had a great afinity for computers, even my mother often tells me that I wanted to create robots and she talked about it all the time.",
+    },
+    {
+      contentType: "paragraph",
+      content:
+        "My first contact with software development was more or less in 2014, I tried to do a video game programming course with Unity using monodevelop and C#, but honestly it was too difficult, today I know that it is one of the most complex areas in the software development, but the good side is that the experience only gave me determination to start something simpler.",
+    },
+    {
+      contentType: "paragraph",
+      content:
+        "shortly after I started learning C++ with the book <a href='https://www.amazon.com/Primer-5th-Stanley-B-Lippman/dp/0321714113' target='_blank' rel='noopener noreferrer'>C++ Primer Fifth Edition</a>. What I like most about software development is the feeling of creating things from nothing, it is almost magical, you write a spell and something appears that a few hours ago did not exist and with which you can interact, and above all, it solves a problem.",
+    },
+    {
+      contentType: "note",
+      content:
+        "This isn't actually the first book I read about programming, but it's the most important for me.",
+    },
+    {
+      contentType: "paragraph",
+      content:
+        "Over time I also learned other technologies such as Python, GoLang, C#, HTML, JS, React, Vue, TypeScript, PHP and Laravel. Although I have also tried other things such as video game development with the GML or Python using Godot, or development for Arduino with C.",
+    },
+  ],
+};
